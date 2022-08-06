1429dc92338097120f2379fa03df641f13afe396
Gly <gabin.lefranc@gmail.com>
2022-08-07 00:56:22 +0200

    Add unexpected shortcut

--- consoleapp/include/expected.h
+++ consoleapp/include/expected.h
@@ -2,8 +2,12 @@
 #include <expected>
 template <class T, class E>
 using expected = std::expected<T, E>;
+template <class E>
+using unexpected = std::unexpected<E>;
 #else
 #include <tl/expected.hpp>
 template <class T, class E>
 using expected = tl::expected<T, E>;
+template <class E>
+using unexpected = tl::unexpected<E>;
 #endif
