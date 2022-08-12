b75b2510dc767d2b8f6a2ac2d37fdc87c2378666
Gly <gabin.lefranc@gmail.com>
2022-08-12 19:48:44 +0200

    remove argumentum dep

--- xmake.lua
+++ xmake.lua
@@ -3,6 +3,6 @@ add_rules("mode.debug", "mode.release")
 add_requires("libarchive")
 add_requires("fmt")
 add_requires("tl_expected")
-add_requires("vcpkg::argumentum", {alias = "argumentum"})
+
 
 includes("qtapp", "consoleapp", "zfiles")
