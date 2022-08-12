fcbac03303f80119a33fb9dc4b4592067b444b05
Gly <gabin.lefranc@gmail.com>
2022-08-12 19:49:33 +0200

    Setup llvm toolchain in xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,10 @@
 includes("xmake/**.lua")
 add_rules("mode.debug", "mode.release")
+
 add_requires("libarchive")
 add_requires("fmt")
 add_requires("tl_expected")
 
+llvm_toolchain("LLVM15.0.0", "macosx")
 
 includes("qtapp", "consoleapp", "zfiles")
