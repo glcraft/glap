2c60fbda3b8f0d2702ce6199d83c1f7374ff7fe3
Gly <gabin.lefranc@gmail.com>
2022-08-14 17:23:10 +0200

    Change xmake script for a static lib

--- consoleapp/xmake.lua
+++ consoleapp/xmake.lua
@@ -1,6 +1,17 @@
-target("consoleapp")
-    set_kind("binary")
-    set_languages("cxxlatest", "clatest")
+target("glap")
+    set_kind("static")
+    set_languages("cxx20")
     add_files("src/*.cpp")
+    add_headerfiles("include/**.h")
     add_includedirs("include")
-    add_packages("fmt", "tl_expected")
+    on_load(function (target)
+        import("lib.detect.check_cxsnippets")
+        local has_std_format = check_cxsnippets("static_assert(__cpp_lib_format >= 201907L)")
+        local has_std_expected = check_cxsnippets("static_assert(__cpp_lib_expected)")
+        if not has_std_format then
+            target:add("packages", "fmt")
+        end
+        if not has_std_expected then
+            target:add("packages", "tl_expected")
+        end
+    end)
