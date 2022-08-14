34e72111667d0f143a631b64bc64299011f3bdb3
Gly <gabin.lefranc@gmail.com>
2022-08-14 02:38:55 +0200

    update demo

--- consoleapp/src/main.cpp
+++ consoleapp/src/main.cpp
@@ -10,7 +10,7 @@ auto parse_argumnts(int argc, char **argv) -> cmd::result::PosExpected<cmd::resu
 {
     cmd::Parser parser;
     auto& cmd_compress = parser.make_command("compress", 'c').set_description("Compress files and directories");
-    cmd_compress.make_argument("output", 'o').set_description("Output file");
+    cmd_compress.make_argument("output", 'o').set_description("Output file").set_required(true);
     cmd_compress
         .make_argument("check", 'c')
         .set_validator([](std::string_view value) -> bool {
@@ -22,6 +22,7 @@ auto parse_argumnts(int argc, char **argv) -> cmd::result::PosExpected<cmd::resu
     cmd_compress.make_flag("verbose", 'v').set_description("Verbose mode").set_max(3);
     cmd_compress.make_flag("Flag1", 'a').set_description("Test flag 1");
     cmd_compress.make_flag("Flag2", 'b').set_description("Test flag 2");
+    parser.set_global_command("compress");
     parser.make_command("extract", 'x').set_description("Extract files from compressed file");
     parser.make_command("list", 'l').set_description("Explore compressed file");
 
