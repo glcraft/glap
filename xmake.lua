eb28da6c78fbb01687d8549e821d4981aca8c6ea
Gly <gabin.lefranc@gmail.com>
2022-08-14 18:39:55 +0200

    add requires to the target

--- xmake.lua
+++ xmake.lua
@@ -1,3 +1,5 @@
+add_requires("fmt", {optional = true})
+add_requires("tl_expected", {optional = true})
 target("glap")
     set_kind("static")
     set_languages("cxx20")
@@ -9,10 +11,10 @@ target("glap")
         local has_std_format = check_cxsnippets("static_assert(__cpp_lib_format >= 201907L)")
         local has_std_expected = check_cxsnippets("static_assert(__cpp_lib_expected)")
         if not has_std_format then
-            target:add("packages", "fmt")
+            target:add("packages", "fmt", {public = true})
         end
         if not has_std_expected then
-            target:add("packages", "tl_expected")
+            target:add("packages", "tl_expected", {public = true})
         end
     end)
 
