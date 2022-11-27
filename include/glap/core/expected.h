b1dee1ca2742cc4ee0f0a41ddaba203f85191725
Arthur LAURENT <arthur.laurent4@gmail.com>
2022-11-27 23:02:13 +0100

    Fix concept usage on apple

--- include/glap/core/expected.h
+++ include/glap/core/expected.h
@@ -23,25 +23,22 @@ namespace glap
 } // namespace glap
 #endif
 
+#if defined(__cpp_lib_concepts) && __cpp_lib_concepts >= 201806L
 #include <concepts>
-
-#ifdef __APPLE__
-    #include <TargetConditionals.h>
-    #ifdef TARGET_OS_IPHONE
-        #include <type_traits>
-    #endif
+#else
+#include <type_traits>
 #endif
 
 
 namespace glap
 {
     namespace details {
-    #ifdef TARGET_OS_IPHONE
-        template <typename T, typename U>
-        concept convertible_to = std::is_convertible<U, T>;
-    #else
+    #if defined(__cpp_lib_concepts) && __cpp_lib_concepts >= 201806L
         template <typename T, typename U>
         concept convertible_to = std::convertible_to<T, U>;
+    #else
+        template <typename T, typename U>
+        concept convertible_to = std::is_convertible<U, T>;
     #endif
     }
 
