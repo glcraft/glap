8c1d22c288742bebd42b1431eaca8090fb57c87e
Arthur LAURENT <arthur.laurent4@gmail.com>
2022-11-27 22:55:39 +0100

    Fix compilation on iOS

--- include/glap/core/expected.h
+++ include/glap/core/expected.h
@@ -25,10 +25,29 @@ namespace glap
 
 #include <concepts>
 
+#ifdef __APPLE__
+    #include <TargetConditionals.h>
+    #ifdef TARGET_OS_IPHONE
+        #include <type_traits>
+    #endif
+#endif
+
+
 namespace glap
 {
+    namespace details {
+    #ifdef TARGET_OS_IPHONE
+        template <typename T, typename U>
+        concept convertible_to = std::is_convertible<U, T>;
+    #else
+        template <typename T, typename U>
+        concept convertible_to = std::convertible_to<T, U>;
+    #endif
+    }
+
     template <class T>
     concept IsExpected = requires(T t) {
+
         {t.value()} -> std::convertible_to<typename T::value_type>;
         {t.error()} -> std::convertible_to<typename T::error_type>;
     };
