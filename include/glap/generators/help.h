52960cf700ec1021353827a7b0746d6b2190e430
Gly <gabin.lefranc@gmail.com>
2022-11-29 00:06:32 +0100

    update help to work with the new model

--- include/glap/generators/help.h
+++ include/glap/generators/help.h
@@ -1,11 +1,11 @@
 #pragma once
-#include "core/discard.h"
-#include "core/utils.h"
+#include "../core/discard.h"
+#include "../core/utils.h"
 #include <concepts>
 #include <optional>
 #include <string_view>
 #include <type_traits>
-namespace glap {
+namespace glap::generators {
     namespace help {
         static constexpr std::string_view INPUTS_NAME = "INPUTS";
         static constexpr int PADDING = 4;
@@ -16,29 +16,27 @@ namespace glap {
             && std::same_as<std::remove_cvref_t<decltype(T::long_description)>, std::string_view>;
         template <class T>
         concept IsInputs = (T::name == INPUTS_NAME);
-        namespace model 
-        {
-            template<StringLiteral Short>
-            struct Description {
-                static constexpr std::string_view short_description = Short;
-            };
-            template<StringLiteral Short, StringLiteral Long>
-            struct FullDescription : Description<Short> {
-                static constexpr std::string_view long_description = Long;
-            };
 
-            template<StringLiteral Name, IsDescription Desc> 
-            struct Argument : Desc {
-                static constexpr std::string_view name = Name;
-            };
-            template<StringLiteral Name, IsDescription Desc, class ...Params>
-            struct Command : Desc {
-                static constexpr auto name = Name;
-            };
-            template<StringLiteral Name, IsDescription Desc, class ...Commands>
-            struct Program : Desc
-            {};
-        }
+        template<StringLiteral Short>
+        struct Description {
+            static constexpr std::string_view short_description = Short;
+        };
+        template<StringLiteral Short, StringLiteral Long>
+        struct FullDescription : Description<Short> {
+            static constexpr std::string_view long_description = Long;
+        };
+
+        template<StringLiteral Name, IsDescription Desc> 
+        struct Argument : Desc {
+            static constexpr std::string_view name = Name;
+        };
+        template<StringLiteral Name, IsDescription Desc, class ...Params>
+        struct Command : Desc {
+            static constexpr auto name = Name;
+        };
+        template<StringLiteral Name, IsDescription Desc, class ...Commands>
+        struct Program : Desc
+        {};
     }
     template<class FromHelp, class FromParser> 
     struct Help
