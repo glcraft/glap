f067c08ef5ed0d5dfa7dc51f6cdee4ac4e83306c
Gly <gabin.lefranc@gmail.com>
2022-11-28 23:25:48 +0100

    add print

--- include/glap/core/fmt.h
+++ include/glap/core/fmt.h
@@ -8,11 +8,13 @@
 namespace glap {
     using std::format;
     using std::format_to;
+    using std::print;
 }
 #else
 #include <fmt/format.h>
 namespace glap {
     using fmt::format;
     using fmt::format_to;
+    using fmt::print;
 }
 #endif
