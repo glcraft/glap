e165b54ae3d2b7a6bab852c4401eb44f4b2a9a70
Gly <gabin.lefranc@gmail.com>
2022-12-01 12:01:50 +0100

    inside project, use glap deps instof package

--- make_my_glap/xmake.lua
+++ make_my_glap/xmake.lua
@@ -1,8 +1,11 @@
-add_requires("glap")
+-- add_requires("glap")
 add_requires("yaml-cpp")
 
 target("make-my-glap")
     set_kind("binary")
+    set_languages("cxx20")
     add_files("src/*.cpp")
     add_includedirs("include")
-    add_packages("glap", "yaml-cpp")
+    add_packages("yaml-cpp")
+    add_options("use_tl_expected", "use_fmt")
+    add_deps("glap")
