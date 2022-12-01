124448c64ad166819c70b2bd84499796e219a0bb
Gly <gabin.lefranc@gmail.com>
2022-12-01 23:19:51 +0100

    add arguments

--- make_my_glap/include/glap_def.h
+++ make_my_glap/include/glap_def.h
@@ -1,3 +1,4 @@
+#include "glap/core/discard.h"
 #include "glap/core/utils.h"
 #include "glap/model.h"
 #include <glap/glap.h>
@@ -8,12 +9,17 @@ namespace args
     inline auto path_exists (std::string_view path) -> bool {
         return std::filesystem::exists(path);
     };
+    inline auto is_type (std::string_view path) -> bool {
+        return path == "header";
+    };
 
-    using param_yaml_t = glap::model::Parameter<glap::Names<"yaml", 'y'>, glap::discard, path_exists>;
+    using param_yaml_t = glap::model::Parameter<glap::Names<"yaml", 'c'>, glap::discard, path_exists>;
 
-    using param_output_t = glap::model::Parameter<glap::Names<"output", 'o'>, glap::discard, glap::discard>;
+    using param_type_t = glap::model::Parameter<glap::Names<"type", 't'>, glap::discard, is_type>;
 
-    using command_generate_t = glap::model::Command<glap::Names<"generate", 'g'>, param_yaml_t, param_output_t>;
+    using param_output_t = glap::model::Parameter<glap::Names<"output", 'o'>>;
+
+    using command_generate_t = glap::model::Command<glap::Names<"generate", 'g'>, param_yaml_t, param_type_t, param_output_t>;
 
     using program_t = glap::model::Program<"make_my_glap", glap::model::DefaultCommand::FirstDefined, command_generate_t>;
 }
