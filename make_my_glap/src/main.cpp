4518e42e9a4301a161cbec3319e36752b9ec9b13
Gly <gabin.lefranc@gmail.com>
2022-12-01 12:02:57 +0100

    wip mmg main

--- make_my_glap/src/main.cpp
+++ make_my_glap/src/main.cpp
@@ -1,4 +1,27 @@
+#include "glap/parser.h"
+#include <glap_def.h>
+#include <span>
+#include <iostream>
+#include <ranges>
+
+
 
 int main(int argc, char** argv) {
-
+    // store arguments in a vector of string_view
+    std::vector<std::string_view> args;
+    for (int i = 0; i < argc; ++i) {
+        args.emplace_back(argv[i]);
+    }
+    auto args_result = glap::parser<args::program_t>(args);
+    if (!args_result) {
+        std::cerr << args_result.error().to_string() << std::endl;
+        return 1;
+    }
+    auto program = args_result.value();
+    auto &command = std::get<args::command_generate_t>(program.command);
+    if (!command.get_argument<"yaml">().value) {
+        std::cout << "yaml path is not set" << std::endl;
+    }
+    return 0;
+    // do something with program
 }
